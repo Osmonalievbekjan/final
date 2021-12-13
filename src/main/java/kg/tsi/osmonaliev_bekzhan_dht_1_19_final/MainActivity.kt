a991cc1230e2038ba008f470f56c2e304f00f96b
bykanykei <92913132+bykanykei@users.noreply.github.com>
2021-12-13 14:44:48 +0600

    commit1

---
+++ src/main/java/kg/tsi/osmonaliev_bekzhan_dht_1_19_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.tsi.osmonaliev_bekzhan_dht_1_19_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
